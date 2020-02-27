85c0ee99813ddb1e78edfa05e78c17b003e7ae66
Alexis Riot <riotalexis35@gmail.com>
2020-02-27 11:47:11 +0100

    #4 - Affichage des données stockées

--- project/app/Http/Controllers/IndexController.php
+++ project/app/Http/Controllers/IndexController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        return view('index');
+        return view('index')
+            ->with('posts_list', Post::all());
     }
 }
--- project/resources/views/index.blade.php
+++ project/resources/views/index.blade.php
@@ -3,21 +3,31 @@
 @section('content')
     <div class="container">
         <div class="row mb-2">
-            <!-- Post 1 -->
-            <div class="col-md-6">
-                <div class="row border mb-4 shadow-sm h-md-250">
-                    <div class="col p-4 d-flex flex-column position-static">
-                        <h3 class="mb-0">Title</h3>
-                        <div class="mb-1 text-muted">Date</div>
-                        <p class="card-text mb-auto">Some content limited to 200 characters...</p>
-                        <a href="#" class="stretched-link">Voir le post</a>
-                    </div>
-                    <div class="d-lg-block">
-                        <img class="bd-placeholder-img" width="200" height="250" src="https://www.webnode.com/blog/wp-content/uploads/2016/10/Blog-intro.jpg">
+            @if ($posts_list->count() > 0)
+                @foreach ($posts_list as $post)
+                <!-- Post {{ $post->id }} -->
+                <div class="col-md-6">
+                    <div class="row border mb-4 shadow-sm h-md-250">
+                        <div class="col p-4 d-flex flex-column position-static">
+                            <h3 class="mb-0">{{ $post->title }}</h3>
+                            <div class="mb-1 text-muted">{{ $post->created_at->diffForHumans() }}</div>
+                            <p class="card-text mb-auto">{!! Str::limit($post->content, 200, ' ...') !!}</p>
+                            <a href="#" class="stretched-link">Voir le post</a>
+                        </div>
+                        <div class="d-lg-block">
+                            <img class="bd-placeholder-img" width="200" height="250" src="https://www.webnode.com/blog/wp-content/uploads/2016/10/Blog-intro.jpg">
+                        </div>
                     </div>
                 </div>
-            </div>
-            <!-- End Post 1 -->
+                <!-- End Post {{ $post->id }} -->
+                @endforeach
+            @else
+                <div class="col-md-12">
+                    <div class="alert alert-info" role="alert">
+                        Aucun post n'a été publié sur le blog!
+                    </div>
+                </div>
+            @endif
         </div>
     </div>
 @endsection
