f08dd2841c45d291ecd131f340e32c94ec5b299c
Alexis Riot <riotalexis35@gmail.com>
2020-02-27 11:33:42 +0100

    #3 - Modification de notre architecture

--- project/resources/views/layouts/app.blade.php
+++ project/resources/views/layouts/app.blade.php
@@ -55,10 +55,13 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('user') }}">
+                                        {{ __('Mon compte') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ __('Déconnexion') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
--- project/routes/web.php
+++ project/routes/web.php
@@ -11,10 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'IndexController@index')->name('index');
+Route::get('/user', 'UserController@index')->name('user');
